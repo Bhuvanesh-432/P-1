0ab3b353489c81644541d2807cd1f30e8abee2bb
Bhuvanesh-432 <130557316+Bhuvanesh-432@users.noreply.github.com>
2026-06-24 13:22:59 +0530

    new files

---
+++ simple-frontend/script.js
@@ -0,0 +1,29 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+body{
+    background:#1e293b;
+    color:white;
+    font-family:Arial;
+}
+
+.container{
+    text-align:center;
+    margin-top:150px;
+}
+
+button{
+    padding:10px 25px;
+    background:orange;
+    border:none;
+    color:white;
+    cursor:pointer;
+    margin-top:20px;
+}
+
+button:hover{
+    background:red;
+}
